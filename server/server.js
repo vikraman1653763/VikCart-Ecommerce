020cc27e79d6d6ebd12b1e35b1ff6f62cb49d564
vikraman1653763 <vikraman1653763@gmail.com>
2025-09-12 14:06:44 +0530

    12-09-2025

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 4000;
 
 await connectDB();
 // allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://vik-cart-ecommerce.vercel.app"];
 
 app.post(
   "/stripe",
